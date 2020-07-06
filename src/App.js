a04a0114cf4c8a5f4eccd2005ca84dc0dc1a4179
engine200 <66384534+engine200@users.noreply.github.com>
2020-07-06 16:04:52 -0500

    assignment1

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
 
   render() {
     console.log(this.state);
+    //for tomorrow, create a subcomponent (like WordComponent) that displays the length of the array in this.state.schoolData
     return (
       <div className="App">
         <header className="App-header">
