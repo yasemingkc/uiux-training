4407408aa149f359f3065de6abc435bb227ca577
engine200 <66384534+engine200@users.noreply.github.com>
2020-07-06 15:07:41 -0500

    add files

---
+++ build/precache-manifest.607c820d843cc03c826da989e28d0940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae846b3ab8cf09ca6f9d7e73ca53f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62d2311fe9cd53095ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ecf24cd74552f6edb9e",
+    "url": "/static/js/2.d8b44b9d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.d8b44b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62d2311fe9cd53095ad",
+    "url": "/static/js/main.73418a43.chunk.js"
+  },
+  {
+    "revision": "e8ebec01f131f819717c",
+    "url": "/static/js/runtime-main.c2054a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import _ from 'lodash';
+import logo from './logo.svg';
+import './App.css';
+import WordComponent from './WordComponent';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: 'initialstate'
+    }
+  }
+
+  render() {
+    console.log(_.lowerCase('ABC'));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => {
+            fetch('/ping')
+            .then((res) => res.json())
+            .then((data) => this.setState({word: data.word}))
+          }}>Get Data</button>
+          <WordComponent data={this.state.word} />
+        </header>
+  
+      </div>
+    );
+  }
+}
+
+export default App;
