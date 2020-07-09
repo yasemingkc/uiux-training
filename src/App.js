572dc50ced928cd8d552f48682cfbe1ee97ec924
engine200 <66384534+engine200@users.noreply.github.com>
2020-07-09 12:18:44 -0500

    Make .App into flex box with 3 children

--- src/App.js
+++ src/App.js
@@ -15,22 +15,29 @@ class App extends React.Component {
     //for tomorrow, create a subcomponent (like WordComponent) that displays the length of the array in this.state.schoolData
     return (
       <div className="App">
-        <header className="App-header">
-          <button
-            onClick={() => {
-              fetch("/sat-scores")
-                .then((res) => res.json())
-                .then((data) => this.setState({ stateSchoolData: data }));
-            }}
-          >
-            {" "}
-            Fetch SAT Scores{" "}
-          </button>
-          <SchoolStats statsSchoolData={this.state.stateSchoolData} />
-        </header>
+        <div className="top-bar" />
+        <div className="statistics">Some statistics</div>
+        <div className="grid" />
       </div>
     );
   }
 }
+
+/*
+
+<header className="App-header">
+  <button
+    onClick={() => {
+      fetch("/sat-scores")
+        .then((res) => res.json())
+        .then((data) => this.setState({ stateSchoolData: data }));
+    }}
+  >
+    {" "}
+    Fetch SAT Scores{" "}
+  </button>
+  <SchoolStats statsSchoolData={this.state.stateSchoolData} />
+</header>
+*/
 
 export default App;
