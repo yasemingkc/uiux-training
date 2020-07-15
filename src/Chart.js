24398db35f08589bb7530465b3fd3775e6f30fc9
engine200 <66384534+engine200@users.noreply.github.com>
2020-07-15 16:42:19 -0500

    Implement highcharts homework

--- src/Chart.js
+++ src/Chart.js
@@ -5,14 +5,17 @@ import _ from "lodash";
 
 // homework: create a scatterplot with xaxis = critical reading mean, y axis = math mean, each dot = 1 school & when you hover over, you get the school name.
 
-const createOptions = (incomingSchoolData) => {
+const createColumnChartOptions = (incomingSchoolData) => {
   let schoolData = _.slice(incomingSchoolData, 0, 5);
   return {
+    credits: {
+      enabled: false,
+    },
     title: {
       text: "5 Schools' Scores",
     },
     xAxis: {
-      categories: _.map(schoolData, (school) => school["School Name"]),
+      categories: _.map(schoolData, "School Name"),
     },
     series: [
       {
@@ -30,12 +33,66 @@ const createOptions = (incomingSchoolData) => {
   };
 };
 
+const createScatterPlotOptions = (incomingSchoolData) => {
+  let series = _.map(incomingSchoolData, (school) => {
+    return {
+      name: _.get(school, "School Name", ""),
+      color: "black",
+      marker: {
+        symbol: "circle",
+        radius: 3,
+      },
+      data: [
+        {
+          x: _.get(school, "Critical Reading Mean"),
+          y: _.get(school, "Mathematics Mean"),
+        },
+      ],
+    };
+  });
+
+  return {
+    credits: {
+      enabled: false,
+    },
+    title: {
+      text: "NYC Schools Math vs CR Scores",
+    },
+    chart: {
+      type: "scatter",
+      zoomType: "xy",
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      scatter: {
+        tooltip: {
+          headerFormat: "<b>{series.name}</b><br>",
+          pointFormat: "CR: {point.x}, Math: {point.y}",
+        },
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Critical reading mean",
+      },
+    },
+    yAxis: {
+      title: {
+        text: "Math mean",
+      },
+    },
+    series,
+  };
+};
+
 class Chart extends React.Component {
   render() {
     return (
       <HighchartsReact
         highcharts={Highcharts}
-        options={createOptions(this.props.data)}
+        options={createScatterPlotOptions(this.props.data)}
       />
     );
   }
